ea0b4b462c21adc5e21382386213126c177fcae1
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-03-06 16:30:41 -0600

    tests passing for invalid shift values

--- src/caesar.js
+++ src/caesar.js
@@ -3,11 +3,39 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
+// *three parameters:
+// input refers to the inputted text to be encoded or decoded.
+// shift refers to how much each letter is "shifted" by. A positive number means shifting to the right (i.e. "A" becomes "D") whereas a negative number means shifting to the left (i.e. "M" becomes "K").
+// encode refers to whether you should encode or decode the message. By default it is set to true.
+
+// *constraints and rules:
+// todo If the shift value is not present, equal to 0, less than -25, or greater than 25, the function should return false.
+// todo Spaces should be maintained throughout, as should other non-alphabetic symbols.
+// todo Capital letters can be ignored.
+// todo If a letter is shifted so that it goes "off" the alphabet (e.g. a shift of 3 on the letter "z"), it should wrap around to the front of the alphabet (e.g. "z" becomes "c").
+
+// *Examples
+// caesar("thinkful", 3); //> 'wklqnixo'
+// caesar("thinkful", -3); //> 'qefkhcri'
+// caesar("wklqnixo", 3, false); //> 'thinkful'
+
+// caesar("This is a secret message!", 8); //> 'bpqa qa i amkzmb umaaiom!'
+// caesar("BPQA qa I amkzmb umaaiom!", 8, false); //> 'this is a secret message!'
+
+// caesar("thinkful"); //> false
+// caesar("thinkful", 99); //> false
+// caesar("thinkful", -26); //> false
+
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    let result = true;
+    if (shift < -25 || shift > 25 || !shift) {
+      return false;
+    }
+
+    return result;
   }
 
   return {
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,29 @@
-// Write your tests here!
+const caesar = require("../src/caesar");
+const { expect } = require("chai");
+
+describe("caesar", () => {
+  let input = "Brian Hook";
+
+  it("1) is a function", () => {
+    expect(caesar).to.be.a("function");
+  });
+
+  it("2) return false for all invalid shift values", () => {
+    const shiftValues = [0, -26, 26, null, undefined];
+    const actual = shiftValues.every((shift) => {
+      console.log(`${shift} - ${caesar(input, shift)}`);
+      return !caesar(input, shift);
+    });
+    expect(actual).to.be.true;
+  });
+
+  // !only for dev test. This should turn into "returns valid string" later when there is a function to test
+  it("3) returns true for all valid shift numbers", () => {
+    const shiftValues = [-25, -1, 1, 25];
+    const actual = shiftValues.every((shift) => {
+      console.log(`${shift} - ${caesar(input, shift)}`);
+      return caesar(input, shift);
+    });
+    expect(actual).to.be.true;
+  });
+});
