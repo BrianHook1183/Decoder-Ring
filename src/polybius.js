16ee7138118816830af91964ddb5674ef1b22518
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-03-07 00:17:27 -0600

    i/j failing test | starting point

--- src/polybius.js
+++ src/polybius.js
@@ -3,11 +3,26 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
-const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+// constraints and rules in mind:
 
+// You are welcome to assume that no additional symbols will be included as part of the input. Only spaces and letters will be included.
+// When encoding, your output should still be a string.
+// When decoding, the number of characters in the string excluding spaces should be even. Otherwise, return false.
+// Spaces should be maintained throughout.
+// Capital letters can be ignored.
+// The letters "I" and "J" share a space. When encoding, both letters can be converted to 42, but when decoding, both letters should somehow be shown.
+// Examples
+// polybius("thinkful"); //> "4432423352125413"
+// polybius("Hello world"); //> '3251131343 2543241341'
+
+// polybius("3251131343 2543241341", false); //> "hello world"
+// polybius("4432423352125413", false); //> "th(i/j)nkful
+// polybius("44324233521254134", false); //> false
+
+const polybiusModule = (function () {
   function polybius(input, encode = true) {
-    // your solution code here
+    const unicode = input.toLowerCase().charCodeAt();
+    return unicode;
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,17 @@
-// Write your tests here!
+const polybius = require("../src/polybius");
+const { expect } = require("chai");
+
+describe.only("polybius()", () => {
+  describe("encoding", () => {
+    it("should maintain spaces", () => {});
+    it("should ignore capital letters", () => {});
+    it.only("should translate the letters i and j to 42", () => {
+      expect([polybius("i"), polybius("j")]).to.equal([42, 42]);
+    });
+  });
+  describe("decoding", () => {
+    it("should maintain spaces", () => {});
+    it("should ignore capital letters", () => {});
+    it("should translate 42 to (i/j)", () => {});
+  });
+});
