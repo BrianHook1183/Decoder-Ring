1e2052bad723d556984062558129afb15f7319a2
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-03-07 20:50:08 -0600

    added failing tests

--- src/substitution.js
+++ src/substitution.js
@@ -3,11 +3,20 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
+// constraints and rules:
+
+// -The input could include spaces and letters as well as special characters such as #, $, *, etc.
+// -Spaces should be maintained throughout.
+// -Capital letters can be ignored.
+// -The alphabet parameter must be a string of exactly 26 characters, which could include special characters such as #, $, *, etc. Otherwise, it should return false.
+// -All of the characters in the alphabet parameter must be unique. Otherwise, it should return false.
+
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    if (alphabet.length !== 26) return false;
+    return true;
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,84 @@
-// Write your tests here!
+const { expect } = require("chai");
+const substitution = require("../src/substitution");
+
+describe.only("substitution()", () => {
+  it("should be a function", () => {
+    expect(substitution).to.be.a("function");
+  });
+
+  describe("error handling", () => {
+    it("should return false if the substitution alphabet is missing", () => {
+      let alphabet = undefined;
+      let input = "thinkful";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.be.false;
+    });
+    it("should return false if the substitution alphabet is not exactly 26 characters", () => {
+      let alphabet = "short";
+      let input = "thinkful";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.be.false;
+    });
+    it("should return false if the substitution alphabet does not contain unique characters", () => {
+      let alphabet = "abcabcabcabcabcabcabcabcyz";
+      let input = "thinkful";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.be.false;
+    });
+  });
+
+  describe("encoding", () => {
+    it("should encode a message by using the given substitution alphabet", () => {
+      let alphabet = "xoyqmcgrukswaflnthdjpzibev";
+      let input = "thinkful";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.equal("jrufscpw");
+    });
+    it("should work with any kind of key with unique characters", () => {
+      let alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+      let input = "message";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.equal("y&ii$r&");
+    });
+    it("should preserve spaces", () => {
+      let alphabet = "xoyqmcgrukswaflnthdjpzibev";
+      let input = "You are an excellent spy";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.equal("elp xhm xf mbymwwmfj dne");
+    });
+    it("should ignore capital letters", () => {
+      let alphabet = "xoyqmcgrukswaflnthdjpzibev";
+      let input = "ThiNkFul";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.equal("jrufscpw");
+    });
+  });
+
+  describe("decoding", () => {
+    let encode = false;
+    it("should decode a message by using the given substitution alphabet", () => {
+      let alphabet = "xoyqmcgrukswaflnthdjpzibev";
+      let input = "jrufscpw";
+      let actual = substitution(input, alphabet, encode);
+      expect(actual).to.equal("thinkful");
+    });
+    it("should work with any kind of key with unique characters", () => {
+      let alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+      let input = "y&ii$r&";
+      let actual = substitution(input, alphabet, encode);
+      expect(actual).to.equal("message");
+    });
+    it("should preserve spaces", () => {
+      let alphabet = "xoyqmcgrukswaflnthdjpzibev";
+      let input = "elp xhm xf mbymwwmfj dne";
+      let actual = substitution(input, alphabet, encode);
+      expect(actual).to.equal("you are an excellent spy");
+    });
+    it("should ignore capital letters", () => {
+      let alphabet = "xoyqmcgrukswaflnthdjpzibev";
+      let input = "jRufScPw";
+      let actual = substitution(input, alphabet);
+      expect(actual).to.equal("thinkful");
+    });
+  });
+});
