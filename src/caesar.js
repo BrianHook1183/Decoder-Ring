62561dacaafc81f825409347912bc8146b838d48
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-03-11 11:25:07 -0600

    refactored comments

--- src/caesar.js
+++ src/caesar.js
@@ -1,8 +1,3 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const caesarModule = (function () {
   function toUnicode(array) {
     return array.map((character) => {
@@ -12,9 +7,11 @@ const caesarModule = (function () {
   }
 
   function caesar(input, shift, encode = true) {
+    // Checks for given shift constraint
     if (shift < -25 || shift > 25 || !shift) {
       return false;
     }
+    // sets decode mode by inverting the shift
     if (encode === false) {
       shift = shift * -1;
     }
@@ -22,10 +19,12 @@ const caesarModule = (function () {
     let inputArray = input.split("");
     let inputNumbers = toUnicode(inputArray);
 
+    // applies shift only to valid characters, which excludes spaces
     let shiftedNumbers = inputNumbers.map((number) => {
       return typeof number === "number" ? number + shift : number;
     });
 
+    // loop correction handles case where the shift goes left of "a" or right of "z"
     let loopCorrectedNumbers = shiftedNumbers.map((number) => {
       if (typeof number === "number") {
         if (number < 97) {
@@ -38,6 +37,7 @@ const caesarModule = (function () {
       return number;
     });
 
+    //converts unicode back into a string for the resulting output
     let outputArray = loopCorrectedNumbers.map((number) => {
       return typeof number === "number" ? String.fromCharCode(number) : number;
     });
