bd2999b6d8b92596728d5e529099747040def2c9
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-03-11 10:46:54 -0600

    rafactored loops and removed comments

--- src/substitution.js
+++ src/substitution.js
@@ -1,19 +1,4 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (e.g., helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
-// constraints and rules:
-
-// -The input could include spaces and letters as well as special characters such as #, $, *, etc.
-// -Spaces should be maintained throughout.
-// -Capital letters can be ignored.
-// -The alphabet parameter must be a string of exactly 26 characters, which could include special characters such as #, $, *, etc. Otherwise, it should return false.
-// -All of the characters in the alphabet parameter must be unique. Otherwise, it should return false.
-
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
-
   function substitution(input, alphabet, encode = true) {
     // substitution alphabet must exist and be exactly 26 characters long.
     if (!alphabet || alphabet.length !== 26) return false;
@@ -36,11 +21,10 @@ const substitutionModule = (function () {
         const encodedChar = subAlphabetArray[charIndex];
         result.push(encodedChar);
       };
-      for (let i = 0; i < inputArray.length; i++) {
-        const char = inputArray[i];
+      inputArray.forEach((char) => {
         // preserves space or encodes character
         char === " " ? result.push(" ") : encode(char);
-      }
+      });
       return result.join("");
     };
 
@@ -51,11 +35,10 @@ const substitutionModule = (function () {
         const decodedChar = realAlphabetArray[charIndex];
         result.push(decodedChar);
       };
-      for (let i = 0; i < inputArray.length; i++) {
-        const char = inputArray[i];
+      inputArray.forEach((char) => {
         // preserves space or encodes character
         char === " " ? result.push(" ") : decode(char);
-      }
+      });
       return result.join("");
     };
 
